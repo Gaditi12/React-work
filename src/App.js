4c5b79bd7c9f3a2e7dad66791dcd3b99d53323ba
aditigupta <aditi.gupta@deeporion.com>
2023-04-19 16:46:23 +0530

    Writing markup with JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Welcome from "./component/Welcome";
 import Button from "./component/Button";
+import AboutPage from "./component/AboutPage";
 function App() {
   return (
     <>
       <Welcome />
       <Button />
+
+      <AboutPage />
     </>
   );
 }
---
+++ src/component/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <>
+      <h3>About</h3>
+      <p>
+        Hello there. <br />
+        How do you do?
+      </p>
+    </>
+  );
+}
+export default AboutPage;
