33c9063f8c57e471cdeb768c36ec57b9d5b698e2
aditigupta <aditi.gupta@deeporion.com>
2023-04-28 16:40:11 +0530

    cleanup function

--- src/component/UseEffect.jsx
+++ src/component/UseEffect.jsx
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from "react";
 function Example() {
   const [count, setCount] = useState(0);
   const [num, setNum] = useState(0);
+  const [widthCount, setWidthCount] = useState(window.screen.width);
+
+  const actualWidth = () => {
+    setWidthCount(window.innerWidth);
+  };
   function handleClick() {
     setCount(count + 1);
   }
@@ -13,22 +18,27 @@ function Example() {
     console.log("hello useEffect"); // second priority
     //alert("you clicked me");
   }, [count]);
+  useEffect(() => {
+    window.addEventListener("resize", actualWidth);
+    return () => {
+      window.removeEventListener("resize", actualWidth);
+    };
+  });
   console.log("Hello outside"); // first priority
   return (
     <>
-      <div
-        style={{
-          backgroundColor: "#024949",
-          height: "30vh",
-          textAlign: "center",
-        }}
-      >
+      <div className="hook">
         <h1>Using useEffect hook</h1>
         {/* <p>You clicked {count} times</p> */}
         <button onClick={handleClick}>Clicked me {count} times</button>
         <br />
         <br />
         <button onClick={handlesecClick}>Clicked me {num} times</button>
+      </div>
+      <br />
+      <div className="hook">
+        <p>The actual size of the window is :</p>
+        <h3>{widthCount}</h3>
       </div>
     </>
   );
