456fe2cec9a2a9ee91df300333d9cfa95833afcb
aditigupta <aditi.gupta@deeporion.com>
2023-05-02 16:58:41 +0530

    form validation with useRef

--- src/UseRef.js
+++ src/UseRef.js
@@ -1,62 +1,42 @@
 import React, { useRef, useState } from "react";
 
 export default function UseRef() {
-  const refEle = useRef("");
-  const [name, setName] = useState("Aditi");
-  console.log(refEle);
-  function handleReset() {
-    setName("");
-    refEle.current.focus();
-  }
+  //const refEle = useRef("");
+  //const [name, setName] = useState("Aditi");
+
+  // console.log(refEle);
+  // function handleReset() {
+  //   setName("");
+  //   refEle.current.focus();
+  // }
+  const [show, setShow] = useState(false);
+  const ref = useRef(null);
+  const submitForm = (e) => {
+    e.preventDefault();
+    const a = ref.current.value;
+    a === "" ? alert("Please fill the data") : setShow(true);
+  };
   return (
     <>
       <h1>Learning useRef</h1>
-      <input
+      {/* <input
         ref={refEle}
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleReset}>Reset</button> */}
+      <div>
+        <form onSubmit={submitForm}>
+          <div>
+            <label htmlFor="name">Enter your name</label>
+            <input type="text" id="name" ref={ref} />
+          </div>
+          <br />
+          <button>Submit</button>
+        </form>
+        <p>{show ? `Your name is ${ref.current.value}` : ""}</p>
+      </div>
     </>
   );
 }
-
-// import { useState, useRef } from 'react';
-
-// export default function Stopwatch() {
-//   const [startTime, setStartTime] = useState(null);
-//   const [now, setNow] = useState(null);
-//   const intervalRef = useRef(null);
-
-//   function handleStart() {
-//     setStartTime(Date.now());
-//     setNow(Date.now());
-
-//     clearInterval(intervalRef.current);
-//     intervalRef.current = setInterval(() => {
-//       setNow(Date.now());
-//     }, 10);
-//   }
-
-//   function handleStop() {
-//     clearInterval(intervalRef.current);
-//   }
-
-//   let secondsPassed = 0;
-//   if (startTime != null && now != null) {
-//     secondsPassed = (now - startTime) / 1000;
-//   }
-
-//   return (
-//     <>
-//       <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
-//       <button onClick={handleStart}>
-//         Start
-//       </button>
-//       <button onClick={handleStop}>
-//         Stop
-//       </button>
-//     </>
-//   );
-// }
